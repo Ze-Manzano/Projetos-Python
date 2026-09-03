8fa8ac7d06e82e07f8df12bdd03be2e12a435e16
José Henrique Manzano <jdepaolamanzano@gmail.com>
2026-09-03 08:54:12 -0300

    Update Jogo de Adivinhação.py

--- Jogo de Adivinhação.py
+++ Jogo de Adivinhação.py
@@ -1,23 +1,67 @@
-print("---------- JOGO DA ADIVINHAÇÃO ----------")
-print("O computador irá gerar um valor aleatório entre 1 e 100")
-print("Sua missão será adivinhar qual é esse valor enquanto o computador te dará algumas dicas")
-print("-----------------------------------------")
-
 import random
+import os
 
-numero_secreto = random.randint(1, 100)
-tentativas = 0
+print("----------------------ADIVINHE O NÚMERO----------------------\n")
+print("O computador irá gerar um número aleatório, tente adivinha-lo\n")
+print("-------------------------------------------------------------")
 
-while True:
-    palpite = int(input("Digite um numero entre 1 e 100: "))
-    tentativas += 1
+print("            Dificuldade: Fácil (1 a 100), Tentativas: 6                  ")  
+print("            Difculdade: Médio (1 a 500), Tentativas: 8                   ")
+print("            Difculdade: Difícil (1 a 1000), Tentativas: 10               ")
+print("            Digite o número da dificuldade desejada:                     ")
+dificuldade = int(input("Escolha a dificuldade: facil(1), medio(2) ou dificil(3): "))
+
+if dificuldade == 1:
+    numero_secreto = random.randint(1, 100)
+elif dificuldade == 2:
+    numero_secreto = random.randint(1, 500)
+else:
+    numero_secreto = random.randint(1, 1000)
+
+palpite = int(input("Digite um número: "))
+tentativas = 1
+
+while palpite != numero_secreto:
     if palpite < numero_secreto:
-        print("Erado! Tente um valor MAIOR!\n")
-
+        print(f"ERRADO!! Tente um número MAIOR!!!")
+        palpite = int(input("Digite novamente "))
+        tentativas += 1
+        if dificuldade == 1 and tentativas > 6:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
+        elif dificuldade == 2 and tentativas > 8:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}") 
+            break
+        elif dificuldade == 3 and tentativas > 10:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
     elif palpite > numero_secreto:
-        print("Erado! Tente um valor MENOR!\n")
+        print(f"ERRADO!! Tente um número MENOR!!!")
+        palpite = int(input("Digite novamente "))
+        tentativas += 1
+        if dificuldade == 1 and tentativas > 6:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
+        elif dificuldade == 2 and tentativas > 8:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}") 
+            break
+        elif dificuldade == 3 and tentativas > 10:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
 
-    else:
-        print(f"\n🎉PARABÉNS, VOCÊ ACERTOU🎉!")
-        print(f"Você acertou em {tentativas} tentativas!")
-        break
+if palpite == numero_secreto:
+    print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
+    print(f"Número secreto: {numero_secreto} ")
+    print(f"Tentativas: {tentativas}")
